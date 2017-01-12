e3f36e1aee44ba2a26e941301849d4242fb6baf8
Bryce Osterhaus <bryce.osterhaus@liferay.com>
2017-01-12 11:34:46 -0800

    Initial Commit

---
+++ index.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+const player = require('play-sound')(opts = {})
+const exec = require('child_process').exec;
+
+const run = function(cmd){
+	const child = exec(cmd, null, function (error, stdout, stderr) {
+		if (stderr !== null) {
+			console.log('' + stderr);
+		}
+		if (stdout !== null) {
+			console.log('' + stdout);
+		}
+		if (error !== null) {
+			console.log('' + error);
+		}
+
+		player.play('horn.mp3', function(err){})
+
+		setTimeout(
+			() => player.play('horn.mp3', function(err){}),
+			150
+		);
+
+		setTimeout(
+			() => player.play('horn.mp3', function(err){}),
+			300
+		);
+	});
+};
+
+run('npm install');
